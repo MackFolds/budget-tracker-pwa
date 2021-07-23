92e4319e22a2c542b8b0cb4cafae86160b74dc61
Mack Folds <treyfolds93@gmail.com>
2021-07-22 21:37:27 -0600

    fix index.html that didnt save

--- public/js/index.js
+++ public/js/index.js
@@ -144,7 +144,7 @@ function sendTransaction(isAdding) {
   });
 }
 
-document.querySelector("#add-btn").onclick = function() {
+document.querySelector("#add-btn").onclick = function () {
   sendTransaction(true);
 };
 
