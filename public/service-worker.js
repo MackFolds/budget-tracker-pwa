3773d794cc0cc1db33b3ae6b671e9ca3aac7852f
Mack Folds <treyfolds93@gmail.com>
2021-08-17 20:13:55 -0600

    update the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,97 +1,84 @@
-const APP_PREFIX = 'my-site-cache-';
-const VERSION = 'v1';
-const CACHE_NAME = APP_PREFIX + VERSION;
-const DATA_CACHE_NAME = 'data-cache-' + VERSION;
+const CACHE_NAME = 'budget-tracker-v1'
+const DATA_CACHE_NAME = 'data-budget-tracker-v1';
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./css/styles.css",
-    "./js/idb.js",
-    "./js/index.js",
-    "./manifest.json",
-    "./icons/icon-72x72.png",
-    "./icons/icon-96x96.png",
-    "./icons/icon-128x128.png",
-    "./icons/icon-144x144.png",
-    "./icons/icon-152x152.png",
-    "./icons/icon-192x192.png",
-    "./icons/icon-384x384.png",
-    "./icons/icon-512x512.png"
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/service-worker.js',
+    '/css/styles.css',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png',
+    '/js/idb.js',
+    '/js/index.js'
 ];
 
-self.addEventListener('install', (event) => {
-    console.log('Installed Service Worker')
-    console.log(event);
+self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Files were cached!')
-                return cache.addAll(FILES_TO_CACHE)
-            })
-            .then(() => self.skipWaiting())
-            .catch((error) => {
-                console.log('error caching files: ', error);
-            })
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Your files were pre-cached successfully');
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-    console.log('Activated Service Worker')
+self.addEventListener('activate', function(event) {
+    console.log('service worker activated')
     event.waitUntil(
-        caches
-            .keys()
-            .then((keyList) => {
-                return Promise.all(
-                    keyList.map((key) => {
-                        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                            console.log('Removing old cache data: ', key);
-                            return caches.delete(key);
-                        }
-                    })
-                );
-            })
-            .then(() => self.clients.claim())
-            .catch((error) => {
-                console.log(error);
-            })
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if(key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Removing old cache data', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
     );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-    console.log(event.request.url);
-    if (event.request.url.includes('/api')) {
+    if (event.request.url.includes('/api/transaction')) {
         event.respondWith(
-            caches
-                .open(DATA_CACHE_NAME)
-                .then(cache => {
-                    console.log(cache);
-                    return fetch(event.request)
-                        .then(response => {
-                            if (response.status === 200) {
-                                cache.put(event.request.url, response.clone());
-                            }
-                            return response;
-                        })
-                        .catch(err => {
-                            console.log(err);
-                            return cache.match(event.request);
-                        })
+          caches
+            .open(DATA_CACHE_NAME)
+            .then(cache => {
+              console.log(cache);
+              return fetch(event.request)
+                .then(response => {
+                  if (response.status === 200) {
+                    cache.put(event.request.url, response.clone());
+                  }
+                  return response;
                 })
-                .catch(error => console.log(error))
+                .catch(err => {
+                  console.log(err);
+                  return cache.match(event.request);
+                })
+            })
+            .catch(error => console.log(error))
         );
-    } else {
+      } else {
         event.respondWith(
-            fetch(event.request)
-                .catch(error => {
-                    console.log(error);
-                    return caches.match(event.request).then(response => {
-                        if (response) {
-                            return response;
-                        } else if (event.request.headers.get('accept').includes('text/html')) {
-                            return caches.match(event.request.url);
-                        }
-                    })
-                })
+          fetch(event.request)
+            .catch(error => {
+              console.log(error);
+              return caches.match(event.request).then(response => {
+                if (response) {
+                  return response;
+                } else if (event.request.headers.get('accept').includes('text/html')) {
+                  return caches.match(event.request.url);
+                }
+              })
+            })
         )
-    }
+      }
 });
